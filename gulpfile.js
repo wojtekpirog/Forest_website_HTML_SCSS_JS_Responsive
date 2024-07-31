f9f1e840df27fbe19b5ee51d392aeed5797f9924
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-07-31 23:43:56 +0200

    Finish the basic file structure & compile SCSS using Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,11 @@
 const { src, dest } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+
+function compileSass(sassCompiled) {
+  src("./src/scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("./dist/css"));
+  sassCompiled();
+}
+
+exports.default = compileSass;
