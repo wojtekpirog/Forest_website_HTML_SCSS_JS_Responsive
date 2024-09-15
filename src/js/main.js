2cd778e22e71e9257f526630ec18146accf5aca5
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-09-15 22:10:54 +0200

    Create the "Contact us" page

--- src/js/main.js
+++ src/js/main.js
@@ -43,16 +43,20 @@ const toggleNavbarMenu = () => {
   navbarLinksContainer.classList.toggle("navbar__links--active");
   overlay.classList.toggle("navbar__overlay--active");
 
-  navbarLinksContainer.classList.contains("navbar__links--active") 
-    ? toggleButton.setAttribute("aria-expanded", "true") 
-    : toggleButton.setAttribute("aria-expanded", "false");
-
-  navbarLinksContainer.classList.contains("navbar__links--active") && (navbar.classList.remove("navbar--dark"))
+  if (navbarLinksContainer.classList.contains("navbar__links--active")) {
+    toggleButton.setAttribute("aria-expanded", "true");
+    toggleButton.setAttribute("aria-label", "Zamknij menu nawigacyjne");
+  } else {
+    toggleButton.setAttribute("aria-expanded", "false");
+    toggleButton.setAttribute("aria-label", "Otwórz menu nawigacyjne");
+  }
 }
 
 const closeNavbarMenu = () => {
   overlay.classList.remove("navbar__overlay--active");
   toggleButton.classList.remove("navbar__burger-icon--active");
+  toggleButton.setAttribute("aria-expanded", "false");
+  toggleButton.setAttribute("aria-label", "Otwórz menu nawigacyjne");
   navbarLinksContainer.classList.remove("navbar__links--active");
 }
 
