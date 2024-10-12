3f561f6e437f06b3ac16ee92de18da76309eb8bf
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-10-12 20:04:54 +0200

    Configure a map form Mapbox to help navigate a user to the company's headquarter

--- src/js/contact/map.js
+++ src/js/contact/map.js
@@ -1,18 +1,55 @@
 import { mapBox } from "../main.js";
 
-export const renderMap = () => {
+const companyOffice = [19.89338063987431250, 50.0882954269748];
+
+const mapConfig = {
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v9",
+  center: companyOffice,
+  zoom: 15,
+}
+
+const directionsConfig = {
+  accessToken: "pk.eyJ1Ijoidm95dGFzcyIsImEiOiJjbTI0cHdqZWQwZmNhMnFzZzBsY3JvZmw4In0.qsZMkUu8RMnFYBw1oCohug",
+  unit: "metric",
+  profile: "mapbox/driving",
+  placeholderOrigin: "Twoja lokalizacja",
+  placeholderDestination: "Ul. Jasnogórska 2, Kraków",
+  language: "pl-PL"
+}
+
+const markerConfig = {
+  color: "hsl(144, 73%, 52%)",
+  draggable: false
+}
+
+const renderMap = () => {
   mapboxgl.accessToken = "pk.eyJ1Ijoidm95dGFzcyIsImEiOiJjbTI0cHdqZWQwZmNhMnFzZzBsY3JvZmw4In0.qsZMkUu8RMnFYBw1oCohug";
 
   if (mapBox) {
     mapBox.innerHTML = "";
 
-    const map = new mapboxgl.Map({
-      container: "map",
-      style: "mapbox://styles/mapbox/streets-v9",
-      center: [-74.5, 40],
-      zoom: 10
-    }).on("error", (error) => {
-      console.error(`Error while loading the map: ${error.error.message}`);
-    });
+    const navControls = new mapboxgl.NavigationControl();
+
+    const map = new mapboxgl.Map(mapConfig);
+
+    // Add zoom and rotation controls to the map:
+    map.addControl(navControls, 'top-right');
+
+    // Add a marker to the map according to the coords set by `companyOffice`:
+    const marker = new mapboxgl.Marker(markerConfig)
+      .setLngLat(companyOffice)
+      .addTo(map);
+
+    // Add directions to the map:
+    const directions = new MapboxDirections(directionsConfig);
+
+    // Set the default destination to the company office's location:
+    directions.setDestination(companyOffice);
+
+    // Add the directions to the map:
+    map.addControl(directions, "top-left");
   }
 }
+
+export default renderMap;
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import setFooterYear from "./footer.js";
 import handleScrollSpy from "./homepage/scrollspy.js";
 import renderParkCards from "./offer/parks_grid.js";
-import { renderMap } from "./contact/map.js";
+import renderMap from "./contact/map.js";
 import { renderSlider, getSliderWidth, runSlider, resetSlider, handlePrevSlide, handleNextSlide } from "./homepage/slider.js";
 import { generateCookieAlert, handleCookieAlert, checkCookie } from "./cookie_alert.js";
 import { setInitialCharsCounter, handleFormClear, handleFormSubmit, handleTextarea } from "./contact/form.js";
@@ -70,7 +70,7 @@ const main = () => {
   setFooterYear();
   // Execute this code only on the "Home" page
   if (document.body.dataset.currentPage === "home") {
-    handleScrollSpy(); 
+    handleScrollSpy();
     renderSlider();
     getSliderWidth();
     runSlider();
@@ -82,7 +82,7 @@ const main = () => {
   // Execute this code only on the "Contact" page
   if (document.body.dataset.currentPage === "contact") {
     renderMap();
-    setInitialCharsCounter(); 
+    setInitialCharsCounter();
   }
 }
 
