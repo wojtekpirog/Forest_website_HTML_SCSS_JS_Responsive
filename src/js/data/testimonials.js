deb53be3d9f9f08ab33401e165519095fbef11b1
Wojciech Piróg <wojtekpirog12@interia.pl>
2025-05-23 21:23:17 +0200

    Fix image urls andinstall swiper library

--- src/js/data/testimonials.js
+++ src/js/data/testimonials.js
@@ -1,7 +1,7 @@
 const testimonials = [
   {
     id: 1,
-    imageSource: "./images/personOne.min.jpg",
+    imageSource: "./assets/media/personOne.min.jpg",
     imageAltText: "Młoda kobieta w ciemnych włosach nosząca jasnozielony sweter, na drugim planie łąka oraz drzewo.",
     quoteText: "Forest Group to nowy gracz w branży turystycznej, który jest w stanie tworzyć nieszablonowe rozwiązania. Jakość obsługi klienta jest tu na wysokim poziomie, dzięki czemu żaden klient nie zakończy spotkania z doradcą bez świetnego pomysłu na weekend na łonie natury. Serdecznie polecam!",
     fullName: "Leanne Graham",
@@ -9,7 +9,7 @@ const testimonials = [
   },
   {
     id: 2,
-    imageSource: "./images/personTwo.min.jpg",
+    imageSource: "./assets/media/personTwo.min.jpg",
     imageAltText: "Młoda kobieta o ciemnej karnacji i ciemnych włosach, nosząca ciemnoszary sweter, przed nią laptop, a na drugim planie kuchnia.",
     quoteText: "Forest Group to firma z potencjałem, która pomaga podróżnikom (i nie tylko) poznać otaczającą ich przyrodę od zupełnie nowej strony. Katalog wycieczek jest bogaty i konsekwentnie rośnie, dzięki czemu firma umacnia swoją pozycję w przemyśle turystycznym. Chyba już wiem, do kogo zwrócić się o pomoc w doborze destynacji turystycznej na następny weekend.",
     fullName: "Clementine Bauch",
@@ -17,7 +17,7 @@ const testimonials = [
   },
   {
     id: 3,
-    imageSource: "./images/personThree.min.jpg",
+    imageSource: "./assets/media/personThree.min.jpg",
     imageAltText: "Mężczyzna o ciemnych włosach, noszący jasno-beżowy sweter, pod nim czerwoną koszulę w kratę oraz jasno-niebieskie spodnie jeansowe, siedzi na kamieniu. Na drugim planie inne kamienie oraz śnieg.",
     quoteText: "Jako miłośnik podróżowania, uważam, że każde miejsce jest na swój sposób wyjątkowe, i warto odwiedzić je kilka razy, szczególnie jeśli chodzi o parki. A jeszcze lepiej, gdy osoba, która mieszka w okolicy, odsłania przed tobą historię i kulturę tego miejsca, dzięki czemu jesteś jeszcze bardziej nim zachwycony. Serdecznie polecam wycieczki firmy Forest Group.",
     fullName: "Ervin Howell",
@@ -25,7 +25,7 @@ const testimonials = [
   },
   {
     id: 4,
-    imageSource: "./images/personFour.min.jpg",
+    imageSource: "./assets/media/personFour.min.jpg",
     imageAltText: "Młoda kobieta o ciemnych, długich włosach, nosząca czarną sukienkę, a na drugim planie wysokie, suche trawy",
     quoteText: "Płynna obsługa klienta, fachowe doradztwo, przystępne ceny. To sprawia, że Forest Group buduje swoją markę w oparciu o jakościową współpracy z klientem na zasadzie partnerstwa. Firma rozwija się systematycznie, i stopniowo podnosi poprzeczkę, by w przyszłości stać się liderem w branży turystycznej. Polecam zapoznać się z wachlarzem usług Forest Group, gdyż są one warte swojej ceny.",
     fullName: "Patricia Lebsack",
